9c1299e0bd1e500491e8c36070a05581af0bf543
Wimukthi Anuradha Alwis <wimukthianuradhaalwis@outlook.com>
2016-10-26 06:53:26 +0530

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,19 @@
+<?php
+include_once("divs/top.php");
+?>
+
+<ul>
+<table border="0" >
+<tr><td>
+
+<h1><li><a href="School Of Computing.php" font-color: white >School Of Computing</a></li></h1></td></tr>
+<tr><td>
+<h1><li><a href="#">School Of Managment</a></li></h1></td></tr>
+<tr><td>
+<h1><li><a href="#">School Of Engineering</a></li></h1></td></tr></table>
+</ul>
+
+<?php
+include_once("divs/footer.php");
+
+?>
